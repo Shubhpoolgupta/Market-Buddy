eb3dee2f4671b7cedb661efb73916d9433bbd64d
Shubh Gupta <shubhgupta1694@gmail.com>
2026-04-14 15:59:24 +0530

    feartures section done

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/landing//Header'
 import Hero from '../../components/landing/Hero'
+import Features from '../../components/landing/Features'
 function LandingPage() {
   return (
     <div className='bg-[#ffffff] text-gray-600'>
       <Header/>
-      <main>
+      <main className='mb-[100vh]'>
         <Hero/>
+        <Features/>
       </main>
     </div>
   )
